df49bcc78fd4c9a440e4faa346932f4cec1296d9
Mathilde <mathildelie@gmail.com>
2021-09-29 15:23:26 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const userInfo = require('./information');
+
+console.log(userInfo);
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "Hello Wilders!",
+    e : "oO",
+    T : "U "
+}));
---
+++ information.js
@@ -0,0 +1,7 @@
+const objet = {
+    nom: "Mathilde",
+    campus: "remote",
+}
+
+module.exports = { objet
+};
